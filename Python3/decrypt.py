7b342cbc972d3830949ec6b4f5fbdba594e125c2
guu1 <lulz.ga87@gmail.com>
2018-01-18 17:07:50 +0000

    update

--- Python3/decrypt.py
+++ Python3/decrypt.py
@@ -1,23 +1,26 @@
+import base64
+
+def encode(text):
+        encoded = [ord(i) for i in text]
+        return encoded
+
+def decrypt(text, key):
+	encoded_key = encode(key)
+	decoded_b64 = eval(base64.b64decode(text))
+	#print(decoded)
+	buf = []
+	for x in decoded_b64:
+		encoded_key = list(reversed(encoded_key))
+		for i in encoded_key:
+			x = x - 1 >> i % 12
+		buf.append(x)
+	
+	decrypted_string = ''.join(chr(letter) for letter in buf)
+	print(decrypted_string)
+	# return decrypted_string
+
 ####################################
 #
 # Use: decrypt("Wzk1NjgzMjIsIDEzNDM2OTI4LCAxNDI4NjkxNCwgMTQzNTQ0MzIsIDE0NjgwMTMwLCA0MzkyOTYwLCAxMTUzNDQwMiwgMTQ3NDc2NDgsIDE1MDczMzQ2LCAxNDM1NDQzMiwgMTMyMzgzMzgsIDQ1MjQwMzJd", "key")
 #
 ####################################
-import base64
-
-def decrypt(text, key):
-	def encode(text):
-		array = []
-		for i in text:
-			array.append(ord(i))
-		return array
-	keyEncoded = encode(key)
-	encoded = eval(base64.b64decode(text))
-	buf = []
-	for x in encoded:
-		keyEncoded = list(reversed(keyEncoded))
-		for i in keyEncoded:
-			x = x - 1 >> i % 12
-		buf.append(x)
-	array = list(buf)
-	return ''.join(chr(i) for i in array)
--- Python3/encrypt.py
+++ Python3/encrypt.py
@@ -1,23 +1,30 @@
-####################################
-#
-# Use: encrypt("Hello World!", "key")
-#
-####################################
 import base64
 
+def encode(text):
+        array = [ord(i) for i in text]
+        return array
+    
 def encrypt(text, key):
-	def encode(text):
-		array = []
-		for i in text:
-			array.append(ord(i))
-		return array
-	encoded = encode(text)
-	keyEncoded = encode(key)
-	buf = []
-	for x in encoded:
-		for i in keyEncoded:
-			x = x + 1 << i % 12
-		keyEncoded = list(reversed(keyEncoded))
-		buf.append(x)
-	array = list(buf)
-	return base64.b64encode(str(array).encode("ascii"))
+        text_encoded = encode(text)
+        print(text_encoded)
+        key_encoded = encode(key)
+        buf = []
+        for x in text_encoded:
+                for i in key_encoded:
+                        x = x + 1 << i % 12
+                        print(x)
+                key_encoded = list(reversed(key_encoded))
+                print(key_encoded)
+                buf.append(x)
+
+        encrypt_string = base64.b64encode(str(buf).encode("ascii"))
+        return encrypt_string
+
+
+####################################
+#
+# Uncomment to use: 
+#encrypt("Hello World!", "key")
+#
+####################################
+
