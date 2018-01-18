d707daf3b49f76a28a69aa6882acb4cb6a2ad1d6
guu1 <lulz.ga87@gmail.com>
2018-01-18 17:12:36 +0000

    update

--- Python3/encrypt.py
+++ Python3/encrypt.py
@@ -5,25 +5,23 @@ def encode(text):
         return array
     
 def encrypt(text, key):
-        text_encoded = encode(text)
-        print(text_encoded)
-        key_encoded = encode(key)
+        encoded_text = encode(text)
+        encoded_key = encode(key)
         buf = []
-        for x in text_encoded:
-                for i in key_encoded:
+        for x in encoded_text:
+                for i in encoded_key:
                         x = x + 1 << i % 12
-                        print(x)
-                key_encoded = list(reversed(key_encoded))
-                print(key_encoded)
+                encoded_key = list(reversed(encoded_key))
                 buf.append(x)
 
         encrypt_string = base64.b64encode(str(buf).encode("ascii"))
-        return encrypt_string
+        print(encrypt_string)
+        # return encrypt_string
 
 
 ####################################
 #
-# Uncomment to use: 
+# Uncomment: 
 #encrypt("Hello World!", "key")
 #
 ####################################
